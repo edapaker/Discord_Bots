2252c04b247ab9a704a0ef423e678c1b30a4febb
yspollen <ericspollen@gmail.com>
2020-03-22 12:20:55 -0400

    Letter-H Inspector

---
+++ Letter-H_Inspector/index.js
@@ -0,0 +1,20 @@
+const { tokens } = require('./../tokens.js');
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+// command to make BOT online:
+// nodemon --inspect index.js
+
+client.on('ready', ()=>{
+	console.log("Letter-A Inspector is Online!");
+});
+
+client.on('message', msg=>{
+	if (msg.author.bot) return;
+	if (msg.content.toLowerCase().includes('h')){
+			msg.reply('You\'ve used the letter \'H\'!');
+			return;
+	}
+});
+
+client.login(tokens.H_token);
