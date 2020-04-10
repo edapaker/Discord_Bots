f50d00646a61ca9b7e6da10308a06ca1bcb44462
yspollen <ericspollen@gmail.com>
2020-04-10 00:39:59 -0400

    A to H

--- Letter-H_Inspector/index.js
+++ Letter-H_Inspector/index.js
@@ -6,7 +6,7 @@ const client = new Discord.Client();
 // nodemon --inspect index.js
 
 client.on('ready', ()=>{
-	console.log("Letter-A Inspector is Online!");
+	console.log("Letter-H Inspector is Online!");
 });
 
 client.on('message', msg=>{
